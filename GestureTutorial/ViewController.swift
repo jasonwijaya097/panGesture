7594008f91fa8ec0e1f5d75e241b51dc7d0513f0
Jason Valencius Wijaya <jasonwijaya097@gmail.com>
2019-07-11 14:15:15 +0700

    Initial Commit

---
+++ GestureTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GestureTutorial
+//
+//  Created by Jason Valencius Wijaya on 11/07/19.
+//  Copyright © 2019 Jason Valencius Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
